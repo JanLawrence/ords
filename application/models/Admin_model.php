4dd0d45c29e8ade21c568e4fe415cef62d204173
jdtolentino <jltolentino0106@gmail.com>
2018-11-01 00:27:50 +0800

    Update AddUser
    Update add update user

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -6,25 +6,33 @@ class Admin_model extends CI_Model{
         $this->user = isset($this->session->userdata['user']) ? $this->session->userdata['user'] : ''; //get session
     }
     public function userList(){
-        $this->db->select("ui.user_id,CONCAT(ui.last_name, ', ' ,ui.first_name, ' ', ui.middle_name) name, u.user_type")
+        $this->db->select("ui.user_id,CONCAT(ui.last_name, ', ' ,ui.first_name, ' ', ui.middle_name) name, u.user_type,
+            ui.first_name f_name,ui.last_name l_name, ui.middle_name m_name , ui.email, ui.position, u.username, u.password
+        ")
         ->from("tbl_user_info ui")
         ->join("tbl_user u","ON u.id = ui.user_id","inner");
         $this->db->order_by("ui.last_name");
         $query = $this->db->get();
+    
+        foreach($query->result() as $each){
+            $each->password = $this->encryptpass->pass_crypt($each->password, 'd');
+        }
+
         return $query->result();
     }
     public function saveUser(){
-        //data that will be inserted to tbl_subject
+        //data that will be inserted to tbl_user
         $data = array(
             "username" => $_POST['username'],
-            "password" => $_POST['password'],
+            "password" => $this->encryptpass->pass_crypt($_POST['password']),
             "user_type" => $_POST['usertype'],
             "created_by" => $this->user->id,
             "date_created" => date('Y-m-d H:i:s')
         );
         $this->db->insert('tbl_user',$data); //insert data to tbl_user
         $userid = $this->db->insert_id(); // getting the id of the inserted data
-
+        
+        //data that will be inserted to tbl_user_info
         $data = array(
             "user_id" => $userid,
             "first_name" => $_POST['fname'],
@@ -36,6 +44,28 @@ class Admin_model extends CI_Model{
             "date_created" => date('Y-m-d H:i:s')
         );
         $this->db->insert('tbl_user_info',$data); //insert data to tbl_user_info
+    }
+    public function editUser(){
+
+        //data that will be updated to tbl_user
+        $this->db->set('username', $_POST['username']);
+        $this->db->set('password', $this->encryptpass->pass_crypt($_POST['password']));
+        $this->db->set('user_type', $_POST['usertype']);
+        $this->db->set('modified_by', $this->user->id);
+        $this->db->set('date_modified', date('Y-m-d H:i:s'));
+        $this->db->where('id', $_POST['id']);
+        $this->db->update('tbl_user');
+        
+        //data that will be updated to tbl_user_info
+        $this->db->set('first_name', $_POST['fname']);
+        $this->db->set('middle_name', $_POST['mname']);
+        $this->db->set('last_name', $_POST['lname']);
+        $this->db->set('email', $_POST['email']);
+        $this->db->set('position', $_POST['position']);
+        $this->db->set('modified_by', $this->user->id);
+        $this->db->set('date_modified', date('Y-m-d H:i:s'));
+        $this->db->where('user_id', $_POST['id']);
+        $this->db->update('tbl_user_info');
     }
     public function getAllResearh(){
 		$researcher = $this->user->id;
